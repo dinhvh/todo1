48a9a553d572b4ac29787155c7a3e7952d0b54c7
Hoa V. Dinh <dvh@google.com>
2013-09-05 21:59:52 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,118 @@
+$(function () {
+  setup();
+});
+
+var task_id = 1;
+
+var setup = function() {
+  $('#add-button').text('Add Task');
+  $('#add-button').click(function(event) {
+    addTask(task_id, '');
+    task_id ++;
+  });
+  
+  loadData(function() {
+    console.log($('.task'));
+    addTaskIfNeeded();
+  });
+};
+
+var addTaskIfNeeded = function() {
+  if ($('.task').length == 0) {
+    addTask(task_id, '')
+    task_id ++;
+    var tasktitle = $('.task input[type=text]');
+    tasktitle.blur();
+  }
+};
+
+var addTask = function(task_id, title) {
+  $('#list').append('<div class="task" id="task-' + task_id + '"><input type="checkbox"></input><input type="text" class="form-control" placeholder="New Task"></input></div>');
+  scheduleSave();
+  
+  var tasktitle = $('#task-' + task_id + ' input[type=text]');
+  tasktitle.focus();
+  tasktitle.on('input', function(the_task_id, event) {
+    scheduleSave();
+  }.bind(this, task_id));
+  tasktitle.val(title);
+  tasktitle.keypress(function(e) {
+    if (e.which == 13) {
+      tasktitle.blur();
+    }
+  });
+  
+  var checkbox = $('#task-' + task_id + ' input[type=checkbox]');
+  checkbox.change(function(the_task_id, event) {
+    scheduleSave();
+  }.bind(this, task_id));
+};
+
+var scheduledSave = false;
+
+var scheduleSave = function() {
+  if (scheduledSave) {
+    return;
+  }
+  
+  scheduledSave = true;
+  setTimeout(function() {
+    window.webkitRequestFileSystem(window.PERSISTENT, 5*1024*1024*1024, function(fs) {
+      fs.root.getFile('contents', {create: true}, function(createdEntry) {
+        createdEntry.createWriter(function(writer) {
+          var blob = new Blob([serializedData()], {type: 'text/plain'});
+          writer.onwriteend = function() {
+            console.log('truncated');
+            writer.onwriteend = function() {
+              console.log('written');
+            };
+            writer.write(blob);
+          };
+          writer.truncate(0);
+        });
+      });
+    });
+    //console.log(serializeData());
+    scheduledSave = false;
+  }, 2000);
+};
+
+var loadData = function(callback) {
+  window.webkitRequestFileSystem(window.PERSISTENT, 5*1024*1024*1024, function(fs) {
+    fs.root.getFile('contents', {}, function(fileEntry) {
+      fileEntry.file(function(file) {
+        var reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = function(oFREvent) {
+          fillWithSerializedData(oFREvent.target.result);
+          callback();
+        };
+      }, function() {
+        callback();
+      });
+    }, function() {
+      callback();
+    });
+  });
+};
+
+var serializedData = function() {
+  var tasktitles = $('.task input[type=text]');
+  var data = [];
+  for(var i = 0 ; i < tasktitles.length ; i ++) {
+    data.push(tasktitles[i].value);
+  }
+  
+  return JSON.stringify(data);
+};
+
+var fillWithSerializedData = function(data) {
+  console.log(data);
+  var array = JSON.parse(data);
+  array.forEach(function(value, idx) {
+    addTask(task_id, value);
+    task_id ++;
+  });
+  var tasktitle = $('.task input[type=text]');
+  tasktitle.blur();
+};
